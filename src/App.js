8c768f714575b429f3a05f2e2a9c9cd1b35a1841
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-09-19 16:41:44 +0700

    Sukses Melakukan inputan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from "./logo.svg";
 import react from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -8,15 +7,38 @@ class Calculator extends react.Component {
     super(property);
     this.state = {
       calculator_template: 0,
+      calculator_text: "",
     };
+    this.anon = "";
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.anon += event.target.getAttribute("name");
+    this.setState({ calculator_text: this.anon });
+    console.log(this.anon);
+    console.log(event.target.getAttribute("name"));
+  }
+
+  handleSubmit(event) {
+    alert(event.target.getAttribute("name"));
+    console.log(event.target.name);
   }
 
   render() {
     return (
-      <div class="container d-grid gap-3">
-        <div>Tampilan untuk saat pengoperasian</div>
+      <div class="container d-grid gap-3 text-white">
+        <div class="visible">{this.anon}</div>
         <div class="row justify-content-between">
-          <div class="col bg-dark text-white text-center">1</div>
+          <div
+            class="col bg-dark text-white text-center"
+            value="1"
+            name="1"
+            onClick={this.handleChange}
+          >
+            1
+          </div>
           <div class="col bg-dark text-white text-center">2</div>
           <div class="col bg-dark text-white text-center">3</div>
           <div class="col bg-primary text-white text-center">+</div>
